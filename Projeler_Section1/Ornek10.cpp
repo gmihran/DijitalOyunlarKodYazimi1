b8d908449901e4a6afd0d802b045bfe57ff7ed22
Gözde Mihran Altınsoy <32915384+gmihran@users.noreply.github.com>
2019-11-19 17:09:44 +0300

    Add files via upload

---
+++ Projeler_Section1/Ornek10.cpp
@@ -0,0 +1,28 @@
+#include "pch.h"
+#include <iostream>
+
+int main()
+{
+
+
+
+	//Kullanýcý 0 (sýfýr) girene kadar kullanýcýdan deðer isteyelim
+	//Girdiði deðerlerin toplamlarýný ekrana yazdýralým
+
+
+	//Kullanýcý 0 (sýfýr) girene kadar kullanýcýdan deðer isteyelim
+	//Girdiði deðerlerden çiftlerin toplamlarýný ve teklerin toplamlarýný ekrana yazdýralým
+
+
+	//Kullanýcý 0 (sýfýr) girene kadar kullanýcýdan deðer isteyelim
+	//Girdiði deðerlerden negatiflerin toplamlarýný ve pozitiflerin toplamlarýný ekrana yazdýralým
+
+	//Girdiði deðerlerden negatiflerin çarpýmlarýný ve pozitiflerin çarpýmlarýný ekrana yazdýralým
+
+
+	//Girilen sayýnýn tersten okunuþu kendisine eþit mi?
+
+
+	//Üç basamaklý 2 sayýnýn çarpýmý polindrom sayýlarýn en büyüðü olan iki sayýnýn çarpýmlarýný bulalým
+}
+
