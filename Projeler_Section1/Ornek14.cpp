d9faadc7efd8f0cbac18b5cf9f5a097593d208cf
Gözde Mihran Altınsoy <32915384+gmihran@users.noreply.github.com>
2019-12-03 18:47:06 +0300

    Add files via upload

---
+++ Projeler_Section1/Ornek14.cpp
@@ -0,0 +1,92 @@
+#include "pch.h"
+#include <iostream>
+#include <locale.h>
+#include <random>
+#include <time.h>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "turkish");
+	int sayilar[2][4];
+	sayilar[0][0] = 10;
+	sayilar[0][1] = 8;
+	cout << sayilar[0][0] << endl << sayilar[0][1] << endl;
+	//cout << "Sayý:";
+	//cin >> sayilar[0][2];
+	//cout << sayilar[0][2] << endl;
+
+	//Ýki matrisin toplamýný hesaplayan programý yazýnýz.
+	//Matrisler 5*2 boyutunda ve içerisinde 1 ile 100 arasýnda rastgele sayý tutmalarýný saðlayalým.
+	srand(time(NULL));
+	int matris1[5][2], matris2[5][2], i, j;
+	cout << "1. matris:\n";
+	for (i = 0; i <= 4; i++) //satýrlar
+	{
+		for (j = 0; j <= 1; j++) //sütunlar
+		{
+			matris1[i][j] = rand() % 100 + 1;
+			cout << i << "," << j << ":" << matris1[i][j] << "\t";
+		}
+		cout << endl;
+	}
+	cout << "2.matris:\n";
+	for (i = 0; i <= 4; i++) //satýrlar
+	{
+		for (j = 0; j <= 1; j++) //sütunlar
+		{
+			matris2[i][j] = rand() % 100 + 1;
+			cout << i << "," << j << ":" << matris2[i][j] << "\t";
+		}
+		cout << endl;
+	}
+	//matris1 ve matris2 Toplamlarý
+	cout << "Ýki matrisin toplamý\n";
+	for (i = 0; i <= 4; i++) //satýrlar
+	{
+		for (j = 0; j <= 1; j++) //sütunlar
+		{
+			cout << matris1[i][j]+matris2[i][j] << " ";
+		}
+		cout << endl;
+	}
+
+
+	//10*5 boyutlu matris içerisine 20 ile 50 arasýnda rastgele deðerler atayalým ve kullanýcýnýn girdiði sayýyla bu matrisi çarpalým
+	//Matris 2,3 olsaydý ve kullanýcý 2 girseydi;
+//Çýktý:
+/*
+	10*5 Boyutlu Matris:
+	20 22 24
+	21 24 25
+	10*5 Boyutlu Matrisin 2 katý:
+	40 44 48
+	42 48 50
+*/
+	int matris[10][5], sayi;
+	cout << "10*5 Boyutlu Matris:\n";
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			matris[i][j] = rand() % 31 + 20;
+			cout << matris[i][j] << "\t";
+		}
+		cout << endl;
+	}
+	cout << "Sayý:";
+	cin >> sayi;
+	cout << "10 * 5 Boyutlu Matrisin " << sayi <<  " katý:\n";
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			cout << matris[i][j] * sayi << "\t";
+		}
+		cout << endl;
+	}
+
+	//Personel ad, soyad, departman bilgilerini tutan ve 4 personelin kaydýný kullanýcýdan alýp ekrana yazdýran programý yazýnýz.
+	
+	//Dizinin içerisine ilk deðer atamasý yapalým:
+}
+
