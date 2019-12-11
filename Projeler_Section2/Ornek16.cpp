81f7ce9d294e05233ce440b9108f850545602121
Gözde Mihran Altınsoy <32915384+gmihran@users.noreply.github.com>
2019-12-11 12:09:40 +0300

    Update Ornek16.cpp

--- Projeler_Section2/Ornek16.cpp
+++ Projeler_Section2/Ornek16.cpp
@@ -4,7 +4,10 @@
 #include <iostream>
 #include <string>
 #include <locale.h>
+
 using namespace std;
+
+//Fonksiyon tanÄ±mlama:
 void merhaba();
 void yazdir(string kelime);
 void karsilastirma(string kelime1, string kelime2);
@@ -17,42 +20,49 @@ int main()
 	string k;
 	int a, b;
 	merhaba();
-	yazdir("nasýlsýn?");
-	//cout << "Yazdýrýlacak kelime:";
+	yazdir("nasÄ±lsÄ±n?");
+	//cout << "YazdÄ±rÄ±lacak kelime:";
 	//cin >> k;
-	k = "Bugün hava çok soðuk";
+	k = "BugÃ¼n hava Ã§ok soÄŸuk";
 	yazdir(k);
 	karsilastirma("Istanbul", "Istanbul");
 	karsilastirma("Kocaeli", "Istanbul");
-	cout << "2 sayý giriniz:";
+	cout << "2 sayÄ± giriniz:";
 	cin >> a >> b;
 	kucuktenbuyuge(a, b);
 	kuvvet(2, 3);
 	kuvvet(4, 2);
 	kuvvet(a, b);
 	return 0;
-	cout << "Deneme"; //return fonksiyonun deðer geri döndürmesini saðlar. return kodundan sonraki hiçbir kod çalýþmad 
-}
-void merhaba()
-{
-	cout << "Merhaba" << endl;
-}
-void yazdir(string kelime)
-{
-	cout << kelime << endl;
-}
-void karsilastirma(string kelime1, string kelime2)
-{
-	if (kelime1 == kelime2)
-		cout << kelime1 << " " << kelime2 << " Kelimeler ayný" << endl;
-	else
-		cout << kelime1 << " " << kelime2 << " Kelimeler ayný deðil" << endl;
+	//return fonksiyonun deÄŸer geri dÃ¶ndÃ¼rmesini saÄŸlar. return kodundan sonraki hiÃ§bir kod Ã§alÄ±ÅŸmaz.
+	cout << "Deneme"; 
 }
 
-//Kendisine gönderilen iki deðer arasýndaki deðerleri küçükten büyüðe doðru 1'er arttýrarak ekrana yazdýran fonksiyonu tanýmlayýnýz.
-//Ýlk girilen sayý daha büyükse swap(deger1,deger2) (yer deðiþtirme) fonksiyonunu kullanalým
-//Ör: 3 7 girilirse; 3 4 5 6 7
-//Ör: 7 3 girilirse; 3 4 5 6 7
+void merhaba()
+{
+	//Ekrana merhaba yazdÄ±ran fonksiyon
+	cout << "Merhaba" << endl;
+}
+
+void yazdir(string kelime)
+{
+	//Kendisine gÃ¶nderilen kelimeyi ekrana yazdÄ±ran fonksiyon
+	cout << kelime << endl;
+}
+
+void karsilastirma(string kelime1, string kelime2)
+{
+	//Kendisine gÃ¶nderilen iki kelimeyi birbiyle karÅŸÄ±laÅŸtÄ±rÄ±p durumunu ekrana yazdÄ±ran fonksiyon
+	if (kelime1 == kelime2)
+		cout << kelime1 << " " << kelime2 << " Kelimeler aynÄ±" << endl;
+	else
+		cout << kelime1 << " " << kelime2 << " Kelimeler aynÄ± deÄŸil" << endl;
+}
+
+//Kendisine gÃ¶nderilen iki deÄŸer arasÄ±ndaki deÄŸerleri kÃ¼Ã§Ã¼kten bÃ¼yÃ¼ÄŸe doÄŸru 1'er arttÄ±rarak ekrana yazdÄ±ran fonksiyonu tanÄ±mlayÄ±nÄ±z.
+//Ä°lk girilen sayÄ± daha bÃ¼yÃ¼kse swap(deger1,deger2) (yer deÄŸiÅŸtirme) fonksiyonunu kullanalÄ±m
+//Ã–r: 3 7 girilirse; 3 4 5 6 7
+//Ã–r: 7 3 girilirse; 3 4 5 6 7
 void kucuktenbuyuge(int a, int b)
 {
 	int i;
@@ -62,13 +72,13 @@ void kucuktenbuyuge(int a, int b)
 
 	//1.yol:
 	for (i = a; i <= b; i++)
-	//Bu döngüde a deðeri deðiþmez
+	//Bu dÃ¶ngÃ¼de a deÄŸeri deÄŸiÅŸmez
 		cout << i << " ";
 	cout << endl;
 
 	//2.yol:
 	for (a; a <= b; a++)
-	//Bu döngüde a deðeri deðiþir.
+	//Bu dÃ¶ngÃ¼de a deÄŸeri deÄŸiÅŸir.
 		cout << a << " ";
 	cout << endl;
 
@@ -76,12 +86,12 @@ void kucuktenbuyuge(int a, int b)
 
 }
 
-//Kendisine gönderilen iki deðerin kuvvetini hesaplayan fonksiyon
-//Ör: Taban:2 Us:3 girilirse; 2^3= 8 çýktýsýný verecek
-//Ör: Taban:4 Us:2 girilirse; 4^2= 16 çýktýsýný verecek
+//Kendisine gÃ¶nderilen iki deÄŸerin kuvvetini hesaplayan fonksiyon
+//Ã–r: Taban:2 Us:3 girilirse; 2^3= 8 Ã§Ä±ktÄ±sÄ±nÄ± verecek
+//Ã–r: Taban:4 Us:2 girilirse; 4^2= 16 Ã§Ä±ktÄ±sÄ±nÄ± verecek
 void kuvvet(int taban, int us)
 {
-	int sonuc=1; //Çarpmada etkisiz eleman 1 olduðu için sonuç deðerini 1 tanýmladýk
+	int sonuc=1; //Ã‡arpmada etkisiz eleman 1 olduÄŸu iÃ§in sonuÃ§ deÄŸerini 1 tanÄ±mladÄ±k
 	for (int i = 1; i <= us; i++)
 	{
 		sonuc *= taban;
