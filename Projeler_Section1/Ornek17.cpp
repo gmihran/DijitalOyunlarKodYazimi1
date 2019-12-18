372fcafaac4b9e5a10264765353fa45152baaf6f
Gözde Mihran Altınsoy <32915384+gmihran@users.noreply.github.com>
2019-12-18 14:38:20 +0300

    Update Ornek17.cpp

--- Projeler_Section1/Ornek17.cpp
+++ Projeler_Section1/Ornek17.cpp
@@ -14,19 +14,19 @@ void kelimenin_tersi(string *kelime);
 int main()
 {
 	setlocale(LC_ALL, "turkish");
-	//Pointer - İşaretçiler
+	//Pointer - Ä°ÅŸaretÃ§iler
 	//& -> adresi getirir
-	//* -> adresin içerisindeki değeri getirir
-	// pointer tanımlarken başına * sembolü koyulur.Pointer'ın içerisinde tutulan adresteki bilgiye ulaşırken yine önüne * konulur.
-	/*
+	//* -> adresin iÃ§erisindeki deÄŸeri getirir
+	// pointer tanÄ±mlarken baÅŸÄ±na * sembolÃ¼ koyulur.Pointer'Ä±n iÃ§erisinde tutulan adresteki bilgiye ulaÅŸÄ±rken yine Ã¶nÃ¼ne * konulur.
+	
 	ptr();
 	int sayi;
-	cout << "Sayı:";
+	cout << "SayÄ±:";
 	cin >> sayi;
-	cout << "Sayı:" << sayi << endl;
+	cout << "SayÄ±:" << sayi << endl;
 	kat(&sayi);
-	cout << "Sayı:" << sayi << endl;
-	*/
+	cout << "SayÄ±:" << sayi << endl;
+	
 	ptr2();
 	string kelime;
 	cout << "Kelime giriniz..:";
@@ -38,23 +38,23 @@ int main()
 void ptr() {
 	int sayi = 10;
 	int *p = &sayi;
-	cout << "Sayı:" << sayi << endl;
-	cout << "Sayı konum:" << &sayi << endl;
+	cout << "SayÄ±:" << sayi << endl;
+	cout << "SayÄ± konum:" << &sayi << endl;
 	cout << "p:" << p << endl;
 	cout << "p konum:" << &p << endl;
 	*p = 11;
 	cout << "p:" << p << endl;
 	cout << "p+1:" << p + 1 << endl;
 	cout << "*p:" << *p << endl;
-	cout << "Sayı:" << sayi << endl;
+	cout << "SayÄ±:" << sayi << endl;
 }
 
-//Problem: Kullanıcıdan alınan değeri fonksiyon parametre olarak alsın ve bu değeri fonksiyon içerisinde kullanıcının girdiği kata eşitlesin
-//Ör:main içinde 4 girilirse, fonksiyonda 5 girilirse, mainden gelen 4 değeri 20 olacak
-
+//Problem: KullanÄ±cÄ±dan alÄ±nan deÄŸeri fonksiyon parametre olarak alsÄ±n ve 
+//bu deÄŸeri fonksiyon iÃ§erisinde kullanÄ±cÄ±nÄ±n girdiÄŸi kata eÅŸitlesin
+//Ã–r:main iÃ§inde 4 girilirse, fonksiyonda 5 girilirse, mainden gelen 4 deÄŸeri 20 olacak
 void kat(int *p) {
 	int k, sonuc;
-	cout << "Kat değeri:";
+	cout << "Kat deÄŸeri:";
 	cin >> k;
 	sonuc = *p * k;
 	*p = sonuc;
@@ -79,22 +79,25 @@ void ptr2() {
 	cout << "&ptr2:" << &ptr2 << endl;
 }
 
-//Kendisine gönderilen pointer bir kelime tutuyor. Bu kelimeyi tersine dönüştüren fonksiyon:
-//ÖR: kendisine "kedi" kelimesi geldiğinde, kelime "idek" olacak.
+//Kendisine gÃ¶nderilen pointer bir kelime tutuyor. Bu kelimeyi tersine dÃ¶nÃ¼ÅŸtÃ¼ren fonksiyon:
+//Ã–R: kendisine "kedi" kelimesi geldiÄŸinde, kelime "idek" olacak.
 void kelimenin_tersi(string *kelime) {
 	string ters = "";
 	cout << "Kelime:" << *kelime << endl;
 	cout << "Kelime adresi:" << &kelime << endl;
 	int uzunluk = (*kelime).length() - 1;
 	//cout << "Uzunluk:" << uzunluk << endl;
-	//uzunluk değerini kelimenin uzunluğunun 1 eksiğini tutuyor. Bunun nedeni; indis değerinin 0'dan başlamasıdır.
+	//uzunluk deÄŸerini kelimenin uzunluÄŸunun 1 eksiÄŸini tutuyor. Bunun nedeni; indis deÄŸerinin 0'dan baÅŸlamasÄ±dÄ±r.
 
 	for (int i = uzunluk; i >= 0; i--) {
 		ters = ters + (*kelime)[i]; //ters+=(*kelime)[i];
-		//Kelimenin tersini ters diye bir değişkende tutuyoruz. 
+		//Kelimenin tersini ters diye bir deÄŸiÅŸkende tutuyoruz. 
 
 		//*kelime = *kelime + (*kelime)[i]; 
-		//Eğer ters = ters + (*kelime)[i]; *kelime = ters; adımlarının yerine; yukarıdaki adımı gerçekleştirirsek (yani pointer'ın işaret ettiği adresteki verinin yanına kelimenin tersini ekle dersek) kelimemizin yanına tersi eklenmiş olur.
+		//EÄŸer ters = ters + (*kelime)[i]; *kelime = ters; adÄ±mlarÄ±nÄ±n yerine; 
+		//yukarÄ±daki adÄ±mÄ± gerÃ§ekleÅŸtirirsek 
+		//(yani pointer'Ä±n iÅŸaret ettiÄŸi adresteki verinin yanÄ±na kelimenin tersini ekle dersek) 
+		//kelimemizin yanÄ±na tersi eklenmiÅŸ olur.
 	}
 	*kelime = ters;
 	cout << "Fonksiyondaki kelime:" << *kelime << endl;
