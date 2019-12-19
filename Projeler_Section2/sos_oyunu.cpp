6fa4c1fd4583edf6f830fe5f6ec15786bde30d08
Gözde Mihran Altınsoy <32915384+gmihran@users.noreply.github.com>
2019-12-19 11:55:09 +0300

    Update sos_oyunu.cpp

--- Projeler_Section2/sos_oyunu.cpp
+++ Projeler_Section2/sos_oyunu.cpp
@@ -68,7 +68,7 @@ void kullanici() {
 		for (int i = 0; i < 3; i++) {
 			for (int j = 0; j < 3; j++)
 			{
-				if (sos[i][j] == tercih) {
+				if (sos[i][j] == tercih && tercih!='S' && tercih!='O') {
 					satir = i;
 					sutun = j;
 					kontrol = false;
